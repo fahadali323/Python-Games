e17035b5bbe9640538c12972508e16118f8cef1a
fahadali323 <afahad5312@gmail.com>
2023-12-06 12:15:51 -0500

    added jokes program

---
+++ JokeProgram/joke.py
@@ -0,0 +1,13 @@
+#Joke telling program 
+
+snowJoke = input('What do you get when you cross a snowman with a vampire?')
+print(snowJoke)
+
+dentistJoke = input("What do dentists call an astronaut's cavity?")
+print(dentistJoke)
+
+print('Knock knock.')
+response = input()
+print('Interrupting cow.')
+reponse = input()
+print('Interrupting cow ' + reponse + 'MOO!')
